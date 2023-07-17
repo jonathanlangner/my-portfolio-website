37da49af0a4240834962dfcf4dbe9a40bc3c73a7
jonathanlangner <jonlangner@gmail.com>
2023-07-17 10:18:19 -0500

    Updated Portfolio Page to Remove Codepen
    Updated Portfolio Page to Remove Codepen

--- src/pages/PortFolioDoc.jsx
+++ src/pages/PortFolioDoc.jsx
@@ -1,7 +1,8 @@
 import SocialMediaContainer from "../components/SocialMediaContainer";
 import BuyMeACoffeeMobile from "../components/BuyMeACoffeeMobile";
 import { Fragment } from "react";
-import CodeChallenge1 from "../images/codepen-counter-challenge.png";
+import DiceyBusiness from "../images/dicey-business.png";
+import TicTacToe from "../images/tictactoe.png";
 import Chess from "../images/Chess.png";
 import quickDice from "../images/quick-dice-war.png";
 import accessTest from "../images/access-sample.png";
@@ -14,19 +15,34 @@ const PortfolioDoc = function () {
         <div className="container">
          <hr></hr>
          <h1>
-         This Portfolio Page is a showcase of a few of my projects and skills.
+         A Few of My Personal Projects:
          </h1>
          <hr></hr>
          <div className="m-3 d-inline-block">
-         <h3>Intrepid Weather App</h3>
-        <a href="https://interprid-weather-alpha.netlify.app" title="Intrepid Weather">
-          <img className="pic-mobile" src={intrepidWeather} alt="intrepid-weather"></img>
+         
+         <div className="m-3 d-inline-block">
+          <h3>TicTacToe Game</h3>
+          <a  className="m-3" href="https://tictactoe-fbb10f.netlify.app/" title="Accessibility Testing">
+            <img className="pic-mobile" src={TicTacToe} alt="TicTacToe"></img>
+          </a>
+        </div>
+        <div className="m-3 d-inline-block">
+        <h3>JS Chess Game</h3>
+        <a href="https://js-chess-33c20a.netlify.app/"  title="JS Chess Game">
+                    <img className="pic-mobile" src={Chess} alt="JS Chess Game"></img>
+        </a>
+        </div>
+        <hr className="mt-5"></hr>
+        <div className="m-3 d-inline-block">
+        <h3>Dicey Business</h3>
+        <a className="mx-1" href="https://dicey-business-82a7a1.netlify.app/" title="Code Pen Grocery List">
+                    <img className="pic-mobile" src={DiceyBusiness} alt="Dicey Business"></img>
         </a>
         </div>
         <div className="m-3 d-inline-block">
-        <h3>Codepen JS Chess Game Styling</h3>
-        <a href="https://codepen.io/jonlangner/pen/WNzoeQv"  title="JS Chess Game">
-                    <img className="pic-mobile" src={Chess} alt="JS Chess Game"></img>
+         <h3>Intrepid Weather App</h3>
+        <a href="https://interprid-weather-alpha.netlify.app" title="Intrepid Weather">
+          <img className="pic-mobile" src={intrepidWeather} alt="intrepid-weather"></img>
         </a>
         </div>
          <hr className="mt-5"></hr>
@@ -42,13 +58,8 @@ const PortfolioDoc = function () {
             <img className="pic-mobile" src={accessTest} alt="access-test"></img>
           </a>
         </div>
-        <hr></hr>
-        <div className="m-3 d-inline-block">
-        <h3>Codepen Grocery List Styling</h3>
-        <a className="mx-1" href="https://codepen.io/jonlangner/pen/rNJZVxG" title="Code Pen Grocery List">
-                    <img className="pic-mobile" src={CodeChallenge1} alt="CodePen Challenge 1: A Blocked Grocery List"></img>
-        </a>
         </div>
+        <hr></hr>
         
           <hr className="mt-5"></hr>
           <div className="mt-5">
